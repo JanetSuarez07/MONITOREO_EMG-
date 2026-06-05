afde84ff1e99d9729836411ca3f01dd617ece862
JanetSuarez07 <janet.suarezhdz@gmail.com>
2026-06-05 02:45:05 -0600

    Corrigiendo inicializacion de Flask y rutas

--- server.py
+++ server.py
@@ -2,11 +2,13 @@ from flask import Flask, render_template
 from flask_socketio import SocketIO
 import os
 
-app = Flask(__name__, template_folder='.')
+# Al inicializar así, Flask detecta automáticamente la carpeta 'templates' y 'static'
+app = Flask(__name__)
 socketio = SocketIO(app, cors_allowed_origins="*", ping_timeout=60)
 
 @app.route('/')
 def index():
+    # Flask buscará 'index.html' dentro de la carpeta 'templates/'
     return render_template('index.html')
 
 @socketio.on('datos_procesados')
@@ -16,6 +18,10 @@ def handle_data(data):
 @socketio.on('control_sistema')
 def handle_control(data):
     print(f"Control recibido: {data}")
+
+@socketio.on('cambiar_umbral')
+def handle_umbral(data):
+    print(f"Nuevo umbral recibido: {data}")
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 10000))
